84bd363c9869aae4ff85fbe43f9495589c33a632
ARTpositive <72326054+ART-positive@users.noreply.github.com>
2026-01-22 18:58:41 +0300

    refactor: interpolation.clj

--- src/fp3/interpolation.clj
+++ src/fp3/interpolation.clj
@@ -4,39 +4,40 @@
 
 (def ^:private eps 1e-9)
 
-(defn- almost= [a b] (< (Math/abs (- a b)) eps))
+(defn- almost= [a b]
+  (and (number? a) (number? b) (< (Math/abs (- a b)) eps)))
 
-(defmulti interpolate (fn [method points x] method))
+(defmulti interpolate (fn [context] (:algorithm context)))
 
 (defmethod interpolate :linear
-  [_ points x]
-  (let [[p0 p1] (take-last 2 points)]
-    (when (and p0 p1)
-      (let [x0 (:x p0) y0 (:y p0)
-            x1 (:x p1) y1 (:y p1)]
-        (cond
-          (almost= x x0) y0
-          (almost= x x1) y1
-          (almost= x0 x1) (throw (Exception. "Duplicate x"))
-          :else (+ y0 (* (- y1 y0) (/ (- x x0) (- x1 x0)))))))))
+  [{:keys [p1 p2 x]}]
+  (let [x1 (:x p1) y1 (:y p1)
+        x2 (:x p2) y2 (:y p2)]
+    (cond
+      (almost= x x1) y1
+      (almost= x x2) y2
+      (almost= x1 x2) (throw (Exception. "Duplicate x"))
+      :else (+ y1 (* (- y2 y1) (/ (- x x1) (- x2 x1)))))))
 
 (defmethod interpolate :lagrange
-  [_ points x]
+  [{:keys [points x]}]
   (let [n (count points)]
-    (reduce
-     (fn [acc i]
-       (let [xi (:x (nth points i))
-             yi (:y (nth points i))]
-         (+ acc (* yi
-                   (reduce
-                    (fn [term j]
-                      (let [xj (:x (nth points j))]
-                        (if (= i j)
-                          term
-                          (if (almost= xi xj)
-                            (throw (Exception. "Duplicate x in Lagrange"))
-                            (* term (/ (- x xj) (- xi xj)))))))
-                    1.0
-                    (range n))))))
-     0.0
-     (range n))))
+    (when (>= n 2)
+      (reduce
+       (fn [acc i]
+         (let [xi (:x (nth points i))
+               yi (:y (nth points i))]
+           (+ acc
+              (* yi
+                 (reduce
+                  (fn [term j]
+                    (let [xj (:x (nth points j))]
+                      (if (= i j)
+                        term
+                        (if (almost= xi xj)
+                          (throw (Exception. "Duplicate x in Lagrange"))
+                          (* term (/ (- x xj) (- xi xj)))))))
+                  1.0
+                  (range n))))))
+       0.0
+       (range n)))))
