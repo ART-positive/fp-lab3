62bbe3ad27f64da5243a6d7fe392d7f337a52cef
ARTpositive <72326054+ART-positive@users.noreply.github.com>
2026-01-22 18:58:01 +0300

    refactor: core.clj

--- src/fp3/core.clj
+++ src/fp3/core.clj
@@ -4,7 +4,7 @@
             [fp3.interpolation :as interp])
   (:gen-class))
 
-(def algorithms
+(def default-algorithms
   {:linear   {:window 2 :method :linear}
    :lagrange {:window 4 :method :lagrange}})
 
@@ -16,6 +16,10 @@
     :default 1.0
     :parse-fn #(Double/parseDouble %)
     :validate [#(pos? %) "Step must be > 0"]]
+   ["-wg" "--w-lagrange SIZE" "Window size for Lagrange interpolation (integer >= 2)"
+    :id :w-lagrange
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(>= % 2) "Lagrange window size must be at least 2"]]
    ["-h" "--help" "Show help"]])
 
 (defn usage [summary]
@@ -35,13 +39,13 @@
 
 (defn parse-point [line]
   (try
-    (let [[x y] (map #(Double/parseDouble %) (str/split (str/trim line) #"\s+"))]
+    (let [[x y] (map #(Double/parseDouble %)
+                     (str/split (str/trim line) #"\s+"))]
       (interp/->Point x y))
     (catch Exception _ nil)))
 
 (defn ks-for-window [origin step x-min x-max]
-  (let [eps 1e-9
-        k-min (long (Math/ceil (/ (- x-min origin) step)))
+  (let [k-min (long (Math/ceil (/ (- x-min origin) step)))
         k-max (long (Math/floor (/ (- x-max origin) step)))]
     (range k-min (inc k-max))))
 
@@ -55,8 +59,17 @@
            (keep (fn [k]
                    (let [x (+ origin (* k step))]
                      (try
-                       (when-let [y (interp/interpolate (:method config) window x)]
-                         [algo x y k])
+                       (let [result (case (:method config)
+                                      :linear (let [[p0 p1] (take-last 2 window)]
+                                                (interp/interpolate {:algorithm :linear
+                                                                     :p1 p0
+                                                                     :p2 p1
+                                                                     :x x}))
+                                      :lagrange (interp/interpolate {:algorithm :lagrange
+                                                                     :points window
+                                                                     :x x}))]
+                         (when result
+                           [algo x result k]))
                        (catch Exception _ nil)))))
            vec))))
 
@@ -64,48 +77,68 @@
   (let [result (validate-args args)]
     (if (:exit result)
       (do (println (:exit result)) (System/exit 0))
-      (let [{:keys [step]} (:options result)
-            selected (->> [:linear :lagrange]
-                          (filter #(get (:options result) %))
-                          (select-keys algorithms))
-            max-window (apply max (map :window (vals selected)))]
+      (let [{:keys [step w-lagrange]} (:options result)
+            selected-order (->> [:linear :lagrange]
+                                (filter #(get (:options result) %))
+                                vec)
+            base-selected (select-keys default-algorithms selected-order)
+            effective (if (and (contains? base-selected :lagrange) w-lagrange)
+                        (assoc-in base-selected [:lagrange :window] w-lagrange)
+                        base-selected)
+            max-window (if (seq effective) (apply max (map :window (vals effective))) 0)]
+
         (println "Ready. Enter points (x y). Empty line to exit.")
 
-        (loop [points [] origin nil last-ks (zipmap (keys selected) (repeat nil))]
-          (print "> ") (flush)
-          (if-let [line (read-line)]
-            (let [trimmed (str/trim line)]
-              (cond
-                (str/blank? trimmed)
-                (println "Exiting...")
+        (loop [points []
+               origin nil
+               last-ks (zipmap (keys effective) (repeat nil))]
 
-                :else
+          (print "> ") (flush)
+
+          (let [line (read-line)]
+            (cond
+              (nil? line)
+              (do (println "EOF. Exiting.") (System/exit 0))
+
+              (str/blank? (str/trim line))
+              (do (println "Exiting...") (System/exit 0))
+
+              :else
+              (let [trimmed (str/trim line)]
                 (if-let [pt (parse-point trimmed)]
-                  (if (and (seq points) (<= (:x pt) (:x (last points))))
-                    (do (println "x must increase") (recur points origin last-ks))
+                  (if (and (seq points)
+                           (<= (:x pt) (:x (last points))))
+                    (do
+                      (println "x must increase")
+                      (recur points origin last-ks))
+
                     (let [origin' (or origin (:x pt))
                           new-points (conj points pt)
-                          points' (if (> (count new-points) max-window)
-                                    (vec (take-last max-window new-points))
-                                    (vec new-points))
-
+                          points' (->> new-points
+                                       (take-last max-window)
+                                       vec)
                           produced (into {}
-                                         (for [[algo config] selected]
-                                           [algo (produce-for-algo algo config points' step origin' (last-ks algo))]))
+                                         (for [[algo cfg] effective]
+                                           [algo (produce-for-algo
+                                                  algo cfg points'
+                                                  step origin'
+                                                  (get last-ks algo))]))
+                          new-last-ks
+                          (reduce-kv
+                           (fn [m algo pts]
+                             (if (seq pts)
+                               (assoc m algo (last (map last pts)))
+                               m))
+                           last-ks
+                           produced)]
 
-                          new-last-ks (reduce-kv
-                                       (fn [m algo pts]
-                                         (if-let [last-k (last (map last pts))]
-                                           (assoc m algo last-k)
-                                           m))
-                                       last-ks
-                                       produced)]
-
-                      (doseq [[algo _] selected
-                              [_ x y] (produced algo)]
+                      (doseq [[algo pts] produced
+                              [_ x y _] pts]
                         (printf "%s: %.3f %.3f\n" (name algo) x y))
                       (flush)
+
                       (recur points' origin' new-last-ks)))
 
-                  (do (println "Invalid format") (recur points origin last-ks)))))
-            (println "EOF")))))))
+                  (do
+                    (println "Invalid format")
+                    (recur points origin last-ks)))))))))))
